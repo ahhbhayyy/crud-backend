96f7a20783d5d298b7c0a44666d8cbb4b6d14db0
ahhbhayyy <lesnar.abhay@gmail.com>
2025-10-06 18:01:41 +0530

    updated

--- src/db.js
+++ src/db.js
@@ -1,21 +1,25 @@
-import pg from "pg"
-import env from "dotenv"
+import pg from "pg";
+import env from "dotenv";
 
 env.config();
+const { Pool } = pg;
 
+const db = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // needed for Railway
+    })
+  : new Pool({
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT,
+    });
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-  });
-db.connect();
-
-db.on('error', (err) => {
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
-  });
+db.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
 export const query = (text, params) => db.query(text, params);
